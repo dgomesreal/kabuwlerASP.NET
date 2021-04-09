18baa40cd9e0cd21f7c8fa9d734569f918132706
dgomesreal <dgomesreal@gmail.com>
2021-04-08 23:43:01 -0300

    Insert to project "using HtmlAgilityPack"

--- Kabuwler/Controllers/HomeController.cs
+++ Kabuwler/Controllers/HomeController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Kabuwler.Controllers
@@ -20,6 +19,28 @@ namespace Kabuwler.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        private static async Task StartCrawlerAsync()
+        {
+            var url = "https://www.kabum.com.br/";
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var divs = htmlDocument.DocumentNode.Descendants("div")
+                        .Where(node => node.GetAttributeValue("Id", "")
+                        .Equals("pag-detalhes")).ToList();
+
+            foreach(var div in divs)
+            {
+                var title =  div.Descendants("h1").FirstOrDefault().InnerText;
+                var description = div.Descendants("div").Where(div => div.GetAttributeValue("class", "").Equals("content_tab")).FirstOrDefault().InnerText;
+
+            }
         }
     }
 }
--- Kabuwler/Models/Category.cs
+++ Kabuwler/Models/Category.cs
@@ -8,5 +8,10 @@ namespace Kabuwler.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string ImageURL { get; set; }
     }
 }
